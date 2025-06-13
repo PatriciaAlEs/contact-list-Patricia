3fea74ca88a69c843f7c9bfb4406ea29bedf57e5
PatriciaAlEs <patriciaalvarezestevez@gmail.com>
2025-06-13 21:02:30 +0200

    mapeo de contactos ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,32 @@ export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
-	useEffect(()=>{
-		
-		apiServices.getContacts()
-	
+
+	useEffect(() => {
+
+		const getAgendaAsync = async () => {
+			const misContactos = await apiServices.getAgenda();
+			dispatch({ type: 'GET_CONTACTS', payload: misContactos.contacts });
+			console.log(misContactos);
+
+		}
+		getAgendaAsync()
 	}, [])
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Contact List!!</h1>
+			
+			<ul>
+				{store.contactos?.map((contacto, index) => (
+					<div key={index} className="card mb-3">
+						<li>Nombre:{contacto.name} </li>
+						<li>Phone:{contacto.phone} </li>
+						<li>Email:{contacto.email} </li>
+						<li>Address:{contacto.address} </li>
+					</div>
+
+				))}
+			</ul>
 
 		</div>
 	);
--- src/store.js
+++ src/store.js
@@ -1,30 +1,23 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    agenda: null,
+    contactos: []
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
+    case 'GET_AGENDA':
 
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        agenda: action.payload
+      };
+
+    case 'GET_CONTACTS':
+      return {
+        ...store,
+        contactos: action.payload
       };
     default:
       throw Error('Unknown action.');
