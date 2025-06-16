37fb9584e293e01a2d867ca45f0de26b6edd48c8
PatriciaAlEs <patriciaalvarezestevez@gmail.com>
2025-06-16 20:24:26 +0200

    agregado case para add_contacto

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 export const initialStore=()=>{
   return {
     agenda: null,
+    contacto: null,
     contactos: []
   }
 }
@@ -18,6 +19,11 @@ export default function storeReducer(store, action = {}) {
       return {
         ...store,
         contactos: action.payload
+      };
+    case 'ADD_CONTACT':
+      return {
+        ...store,
+        contactos: [...store.contactos, action.payload] // nos actualiza el array de contactos, y nos añade el contacto nuevo (action.payload)
       };
     default:
       throw Error('Unknown action.');
